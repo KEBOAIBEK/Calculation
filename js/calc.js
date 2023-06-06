9f37b9faf872c2b294af177cc1115d31fe809a9a
KEBOAIBEK <102234390+KEBOAIBEK@users.noreply.github.com>
2023-06-07 00:35:37 +0500

    adding

---
+++ js/calc.js
@@ -0,0 +1,22 @@
+    function calculate() {
+        var num1 = parseFloat(document.getElementById("num1").value);
+        var num2 = parseFloat(document.getElementById("num2").value);
+        var operator = document.getElementById("operator").value;
+    
+        var result;
+    
+        if (operator === "+") {
+            result = num1 + num2;
+        } else if (operator === "-") {
+            result = num1 - num2;
+        } else if (operator === "*") {
+            result = num1 * num2;
+        } else if (operator === "/") {
+            result = num1 / num2;
+        } else {
+            result = "Invalid operator";
+        }
+    
+        document.getElementById("result").innerHTML = result;
+    }
+    
